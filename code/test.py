1e800090bad3798daea46b6a60fda1bf2974d4bf
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-03-30 10:55:11 +0530

    update

--- code/test.py
+++ code/test.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def f(name):
     if request.method == 'POST':
         user = request.form['nm']
-    return 'Hello world ' + name
+    return user
 
 if __name__ == '__main__':
     app.debug = True
