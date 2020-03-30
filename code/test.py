277674459d3b4666a0853b3236916d83092982c4
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-03-30 10:25:51 +0530

    update 1

---
+++ code/test.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/login', method = ['POST', 'GET'])
+def f(name):
+    if request.method == 'POST':
+        user = request.form['nm']
+    return 'Hello world ' + name
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+    app.run(debug = True)
