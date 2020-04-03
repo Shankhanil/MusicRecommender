9e350e5844380a73fa76216daa82f7f3c6cb5f83
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-04-03 21:32:06 +0530

    created register.py backend

--- code/registration.py
+++ code/registration.py
@@ -5,26 +5,21 @@ import hashlib
 
 app = Flask(__name__)
 
+def isValid(mail, age):
+    return False
+
 @app.route('/register', methods = ['POST', 'GET'])
 def registerUser():
     if request.method == 'POST':
-        try:
-            name = request.form['name']
-            mailid = request.form['mailid']
-            passw = request.form['passw']
-            securepassw = hashlib.sha256(passw.encode()).hexdigest()
-            print(name, mailid, securepassw)
-            with sql.connect('users.db') as conn:
-                cur = conn.cursor()
-                cur.execute("INSERT INTO user (uname, mailid, passw) VALUES (?, ?, ?)", (name, mailid, securepassw))
-                conn.commit()
-                msg = "Successfully registered"
-        except:
-            conn.rollback()
-            print(sys.exc_info())
-            msg = "Some error occured"
-        finally:
-            return msg
+        name = request.form['name']
+        mail = request.form['mail']
+        password = request.form['password']
+        age = request.form['age']
+        if isValid(mail, age) == True:
+            return "True"
+        
+    return "FAILURE"
 
 if __name__ == "__main__":
     app.run(debug = True)
+    
