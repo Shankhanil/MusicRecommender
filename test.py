7cbbbdf9a71230b2b34ba6d3ca39d51fb7cb45f0
Shankhanil <shankha.rik@gmail.com>
2020-04-09 21:30:53 +0530

    insert search module

--- test.py
+++ test.py
@@ -5,22 +5,32 @@ from flask import Flask, request, render_template, redirect, url_for, session
 from flask_session import Session
 
 
-'''
+
 app = Flask(__name__)
 # Session(app)
 # Session(app)
 
 # app.SECRET_KEY = b'\xdch\x87\x1f\x97\x90\xec\xed\x15O\xef]X\x1eU\xa9\x06\xb6\x0e\x13s\xd3\x95\x07'
 
-@app.route('/set')
+@app.route('/')
 def set():
-    session['key'] = 'abc'
-    return 'ok'
+    # session['key'] = 'abc'
+    # return 'ok'
+    return render_template("home.html")
 
-@app.route('/get')
-def get():
-    # session['key'] = 'value'
-    return session.get('key', 'NOT SET')
+@app.route('/search', methods = ['POST' , 'GET'])
+def search():
+    searchSTR = ""
+    if request.method == 'POST':
+        searchSTR = request.form['search']
+        print(searchSTR)
+        # df = pd.DataFrame(".\\databases\\songList.csv")
+    return searchSTR
+    # return "abc"
+if __name__ == "__main__":
+    app.run(debug = True)
+    
+    
 '''
 if __name__ == "__main__":
     rc = Recommender(userID = 1)
@@ -29,14 +39,13 @@ if __name__ == "__main__":
     rc.listenToSong(song = 'Death Trend Setta')
     rc.listenToSong(song = 'Guerilla Radio')
     
-    '''
+    # 
     rc.listenToSong(song = 'Closer')
     rc.listenToSong(song = 'Believer')
     rc.listenToSong(song = 'Rap God')
-    '''
     # rc.listenToSong(song = 'Numb')
     rc.recommend()
     
     
-    
+'''   
     
