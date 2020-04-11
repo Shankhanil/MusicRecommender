97ef0fb4173011ad319625692235e9949464a3fe
Shankhanil <shankha.rik@gmail.com>
2020-04-11 17:51:37 +0530

    updated templates

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def search():
                     artistL.append(x)
         for x in artistL:
             res2 = res2 + x + ", "
-    return render_template("home.html", search = res, searchArtist = res2)
+    return render_template("_home.html", search = res, searchArtist = res2)
 
 
 @app.route('/login', methods = ['POST', 'GET'])
